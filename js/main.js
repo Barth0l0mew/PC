1c68fcabab966edd60642d03a816908556a6bd96
Barth0l0mew <barth0l0mew@mail.ru>
2022-03-27 22:41:48 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let  days= new Date();
+let day = document.querySelector('.day')
+let hour = document.querySelector('.hour')
+let min = document.querySelector ('.min') 
+let sec = document.querySelector('.sec')
+let btn = document.querySelectorAll('.btn')
+let mod = document.querySelector('.modal')
+let close = document.querySelector('.modal__close')
+let link = document.querySelectorAll('.logo-menu__link')
+console.log(window.pageYOffset)
+function time (){
+    let  days= new Date();
+day.innerHTML  = days.getDate();
+hour.innerHTML = days.getHours();
+min.innerHTML = days.getMinutes();
+sec.innerHTML = days.getSeconds();
+
+}
+function scroll( n){
+    if (n==0){
+        window.scrollBy(0, 1036)
+    }
+
+}
+function modal(){
+    mod.classList.toggle('active')
+}
+setInterval( time , 1000);
+time();
+
+link[0].addEventListener('click',function(event){window.scrollBy(0,1036); event.preventDefault()})
+link[1].addEventListener('click',function(event){window.scrollBy(0,1823); event.preventDefault()})
+link[2].addEventListener('click',function(event){window.scrollBy(0,2458); event.preventDefault()})
+for (let key of btn){
+    key.addEventListener('click', modal);
+}
+close.addEventListener('click',modal)
+
+
